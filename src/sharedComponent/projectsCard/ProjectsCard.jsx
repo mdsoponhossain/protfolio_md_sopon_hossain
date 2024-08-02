0f36da7e9b94d87dc5a0e0b5b77ff752505ccde0
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-08-03 02:34:51 +0600

    updated the dev branch

--- src/sharedComponent/projectsCard/ProjectsCard.jsx
+++ src/sharedComponent/projectsCard/ProjectsCard.jsx
@@ -1,5 +1,6 @@
 import './projectsCard.css'
 const ProjectsCard = () => {
+	
 	return (
 		<div className="card bg-slate-800 p-2 md:p-5 lg:p-10 shadow-md rounded-md card-container">
 			<div className="imgContainer">
