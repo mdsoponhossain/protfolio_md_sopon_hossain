1494a408aaaceedb6ed8841cc08587021c16a51e
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-08-10 02:05:34 +0600

    projects additon --done

--- src/component/homepageComponents/banner/MyProjects.jsx
+++ src/component/homepageComponents/banner/MyProjects.jsx
@@ -1,16 +1,22 @@
+import { useEffect, useState } from "react";
 import HeadingComponent from "../../../sharedComponent/headingComponent/HeadingComponent";
 import ProjectsCard from "../../../sharedComponent/projectsCard/ProjectsCard";
 
 const MyProjects = () => {
+    const [projects, setProjects] = useState([]);
+    useEffect(()=>{
+        fetch('./projects.json')
+        .then(res=>res.json())
+        .then(data=>setProjects(data))
+    },[])
     return (
         <div id="projects" className="h-fit md:py-20 lg:h-fit bg-black bg-opacity-70 pt-10 grid justify-center items-center">
             <HeadingComponent text={'My Projects'}></HeadingComponent>
             <div className="max-w-[1280px] mx-auto  md:mt-10">
                 <div className="px-2 mt-7 md:mt-0 md:mx-4 lg:mx-0 md:m-0 grid gap-5 md:grid-cols-2 md:gap-7 lg:gap-10 lg:grid-cols-2 lg:gap-10">
-                    <ProjectsCard></ProjectsCard>
-                    <ProjectsCard></ProjectsCard>
-                    <ProjectsCard></ProjectsCard>
-                    <ProjectsCard></ProjectsCard>
+                    {
+                        projects?.map((project, index)=><ProjectsCard project={project} key={index}></ProjectsCard>)
+                    }
                 </div>
             </div>
         </div>
--- src/sharedComponent/projectsCard/ProjectsCard.jsx
+++ src/sharedComponent/projectsCard/ProjectsCard.jsx
@@ -1,19 +1,18 @@
 import './projectsCard.css'
-const ProjectsCard = () => {
-	
+const ProjectsCard = ({project}) => {
 	return (
 		<div className="card bg-slate-800 p-2 md:p-5 lg:p-10 shadow-md rounded-md card-container">
 			<div className="imgContainer">
 				<img
-					src="./www.shopify.com_blog_best-ecommerce-sites.png"
+					src={project?.image}
 					alt="img" />
 			</div>
 			<div>
 				<div className="px-2 border border-black bg-slate-800 text-center py-5 text-white grid grid-cols-2 gap-2 md:gap-0 md:grid-cols-4 justify-between ">
-					<div className='custom-btn'>Preview</div>
-					<div className='custom-btn'>Details</div>
-					<div className='custom-btn'>Client</div>
-					<div className='custom-btn'>Server</div>
+					<a target='_blank' href={project?.preview} className='custom-btn'>Preview</a>
+					<div  className='custom-btn'>Details</div>
+					<a target='_blank' href={project?.client} className='custom-btn'>Client</a>
+					<a target='_blank' href={project?.server} className='custom-btn'>Server</a>
 				</div>
 			</div>
 		</div>
