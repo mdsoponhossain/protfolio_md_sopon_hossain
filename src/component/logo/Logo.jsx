546a34f73b6ec2ae1ce2489e2f6a532cd41460c2
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-08-10 01:26:26 +0600

    loading icons additon -- done

--- src/component/logo/Logo.jsx
+++ src/component/logo/Logo.jsx
@@ -1,6 +1,7 @@
+import './logo.css'
 const Logo = ({logoContainerStyle}) => {
     return (
-        <div className={logoContainerStyle}>
+        <div id='logoFont' className={logoContainerStyle}>
             <h3 className="text-3xl">Md</h3>
             <div className="text-xs line-s leading-[12px]">
                 <p>Sopon</p>
