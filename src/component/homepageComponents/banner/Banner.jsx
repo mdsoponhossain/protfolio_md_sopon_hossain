c69e23ec2960d91d599e5bf0781d422286ccab4b
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-08-09 11:28:51 +0600

    image animation added

--- src/component/homepageComponents/banner/Banner.jsx
+++ src/component/homepageComponents/banner/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
             className="hero h-[700px] relative  grid justify-start"
            >
             <div className="hero-overlay bg-opacity-70 absolute grid items-center w-full h-full bg-black">
-                <div className=" h-fit md:h-[70%] xl:w-[1280px] mx-auto text-white grid items-center">
+                <div className=" h-fit md:h-[70%] xl:w-[1280px] mx-auto text-white grid items-center pl-2 md:pl-0">
                     <div className=" h-fit flex-col md:flex-row md:flex justify-between items-center p-2 gap-3">
-                        <div className="">
+                        <div>
                             <h1>Hello, I am <br /><span className="text-5xl banner_name font-bold">Md Sopon Hossain</span></h1>
                             <p className="my-5">I am a MERN Stack developer.I approach coding with enthusiasm</p>
                             {/* custom button */}
@@ -22,7 +22,9 @@ const Banner = () => {
                         </div>
 
 
-                        <img className=" img mt-20 md:mt-0  w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] rounded-full" src="./portfolio_img-removebg-preview.png" alt="Md Sopon Hossain" />
+                        <img 
+                        className=" image-animation img mt-20 md:mt-0  w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] rounded-full pl-6 md:pl-0" 
+                        src="./portfolio_img-removebg-preview.png" alt="Md Sopon Hossain" />
 
                     </div>
                 </div>
