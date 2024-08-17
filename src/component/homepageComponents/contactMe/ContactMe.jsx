60649a00baac8dd5919bd23d082012c0eca33290
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-08-17 14:04:22 +0600

    sending message functionality is completed

--- src/component/homepageComponents/contactMe/ContactMe.jsx
+++ src/component/homepageComponents/contactMe/ContactMe.jsx
@@ -3,32 +3,52 @@ import HeadingComponent from "../../../sharedComponent/headingComponent/HeadingC
 import { IoMdCall } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
 import { IoLocationOutline } from "react-icons/io5";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./contactMe.css"
 const ContactMe = () => {
+    const notify = (text) => toast(text);
     //  sending email function
-    const handle_email = (e) => {
+    const handle_email = async (e) => {
         e.preventDefault();
         const field = e.target;
         const name = field.name.value;
         const email = field.email.value;
         const message = field.message.value;
-        console.log(name, email, message);
         const data = { name, email, message };
-        // https://protfolio-server-md-sopon-hossain.vercel.app
-        // http://localhost:3000
-        fetch('https://protfolio-server-md-sopon-hossain.vercel.app/message', {
+        console.log(data)
+        // fetch('http://localhost:5000/message', {
+        // fetch('https://my-portfolio-server-liard.vercel.app/message', {
+        fetch('https://portfolio-server-production-3379.up.railway.app/message', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                "Content-type": "application/json"
             },
             body: JSON.stringify(data)
         })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (data?.message === 'successful') {
+                    notify("Message is sent to Md Sopon Hossain");
+                    // clearing the form data:
+                    field.name.value = '';
+                    field.email.value = '';
+                    field.message.value = '';
+                } else {
+                    notify("Something wrong! Try again later...");
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                notify("Failed to send message. Please try again later.");
+            });
     }
-
 
 
     return (
         <div id="contact" className="h-fit bg-black py-10 text-white md:py-20 bg-opacity-70">
+            <ToastContainer />
             <HeadingComponent text={'Contact Me'}></HeadingComponent>
             <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-2 mt-5 md:mt-10 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32">
                 <div className="flex flex-col justify-between">
